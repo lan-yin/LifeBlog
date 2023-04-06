739eb45bd8d2966692d3f981199b0da5202ccf9b
lan-yin <jay.braden1128@gmail.com>
2023-04-06 12:20:39 +0800

    navbar developed chakra and routing setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,20 @@
-function App() {
+import { ChakraProvider } from "@chakra-ui/react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+
+const App = () => {
   return (
-    <div className="App">
-      APP
-    </div>
+    <ChakraProvider>
+      <Router>
+        <Navbar />
+        <main>
+          <Routes>
+            
+          </Routes>
+        </main>
+      </Router>
+    </ChakraProvider>
   );
-}
+};
 
 export default App;
